453bb8478cbece33849f628746c97024a45defc7
Alen Bogadi <alenbogadi@proton.me>
2025-03-03 01:14:38 +0100

    Add debugging support

---
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -0,0 +1,39 @@
+return {
+	"rcarriga/nvim-dap-ui",
+	dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+	config = function()
+		local dap, dapui = require("dap"), require("dapui")
+    require("dapui").setup();
+		-- Connect dap with dap-ui
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+		-- php debugging, make sure vscode-php-debug and xdebug are installed and configured properly and configured properly
+		dap.adapters.php = {
+			type = "executable",
+			command = "node",
+			args = { "/home/alen/Programs/vscode-php-debug/out/phpDebug.js" },
+		}
+
+		dap.configurations.php = {
+			{
+				type = "php",
+				request = "launch",
+				name = "Listen for Xdebug",
+				port = 9003,
+			},
+		}
+		-- keybinds
+		vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+	end,
+}
